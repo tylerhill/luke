8659d66ea65e2b3be857dab5e0fe9e5fdc56f8bb
Speedy Beef <speedybeef@Mac-Pro.local>
2015-12-15 18:48:55 -0800

    5 posts and current var

--- routes/blog.js
+++ routes/blog.js
@@ -2,9 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-  var posts = req.app.locals.posts;
-  console.log(posts);
-  res.render('blog', {posts: posts});
+  var dbinst = req.app.get('db');
+  console.log(dbinst);
+  var posts = [];
+  dbinst.each('SELECT rowid, * FROM blog ORDER BY rowid DESC LIMIT 5', function(err,row) {
+    posts.push({id: row.rowid, title: row.title, pBody: row.pBody});
+  },function() {
+    res.render('blog', {posts: posts});
+  });
+});
+
+router.get('/:id', function(req,res,next) {
+  var current = req.params.id;
+  var dbinst = req.app.get('db');
+  console.log(dbinst);
+  var posts = [];
+  dbinst.each('SELECT rowid, * FROM blog ORDER BY rowid DESC LIMIT 5', function(err,row) {
+    posts.push({id: row.rowid, title: row.title, pBody: row.pBody});
+  },function() {
+    res.render('blog', {posts: posts, current: current});
+  });
+
 });
 
 
@@ -16,8 +34,9 @@ router.route('/admin')
     var dbinst = req.app.get('db');
     console.log(dbinst);
     var pBody = req.body.pBody;
+    var title = req.body.title;
     dbinst.serialize(function() {
-      dbinst.run('insert into blog values (?)',pBody);
+      dbinst.run('insert into blog values ((?),(?))',title,pBody);
     });
     res.render('admin');
   });
