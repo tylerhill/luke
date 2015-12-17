ea3ab99867a0292c03499d0753ca68d1ac0749b7
Speedy Beef <speedybeef@Mac-Pro.local>
2015-12-16 18:18:39 -0800

    css and blog index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var dbinst = req.app.get('db');
+  var newPost;
+  dbinst.each('SELECT rowid, * FROM blog ORDER BY rowid DESC LIMIT 1', function(err,row) {
+    newPost = {id: row.rowid, title: row.title, pBody: row.pBody};
+  }, function() {
+    console.log(newPost);
+    res.render('index', { post: newPost });
+  });
 });
 
 module.exports = router;
